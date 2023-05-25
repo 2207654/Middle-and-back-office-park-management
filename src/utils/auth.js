1184b9f0d5b248cd8b0896f41ce0e28820dee742
chaipeng <chaipeng@itcast.cn>
2023-05-25 10:57:00 +0800

    update token_key

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,20 +1,18 @@
 // 专门用来操作cookie的方法包
 // 内部封装了繁琐的操作方法 参数处理 暴露三个函数 get,set,remove
 import Cookies from 'js-cookie'
-
-const TokenKey = 'park'
-
+import { TOKEN_KEY } from '@/constants/KEY'
 // 获取token的方法
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return Cookies.get(TOKEN_KEY)
 }
 
 // 设置方法
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TOKEN_KEY, token)
 }
 
 // 删除方法
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return Cookies.remove(TOKEN_KEY)
 }
